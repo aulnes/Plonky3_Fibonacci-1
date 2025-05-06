cf7151cf78e220b85a543e21886abb40a2d54dfd
aulnes <1214666162@qq.com>
2025-05-06 19:13:01 +0800

    Bump plonky3 version

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 use std::fmt::Debug;
-use std::marker::PhantomData;
+// use std::marker::PhantomData;
 
 use p3_air::{Air, AirBuilder, BaseAir};
-use p3_field::{AbstractField, Field};
+use p3_dft::Radix2DitParallel;
+use p3_field::Field;
+use p3_field::PrimeCharacteristicRing;
 use p3_matrix::Matrix;
 use p3_matrix::dense::RowMajorMatrix;
 
 use p3_challenger::{HashChallenger, SerializingChallenger32};
-use p3_circle::CirclePcs;
+// use p3_circle::CirclePcs;
 use p3_commit::ExtensionMmcs;
 use p3_field::extension::BinomialExtensionField;
-use p3_fri::FriConfig;
+use p3_fri::{FriConfig, TwoAdicFriPcs};
 use p3_keccak::Keccak256Hash;
-use p3_merkle_tree::FieldMerkleTreeMmcs;
-use p3_mersenne_31::Mersenne31;
-use p3_symmetric::{CompressionFunctionFromHasher, SerializingHasher32};
+use p3_merkle_tree::MerkleTreeMmcs;
+// use p3_mersenne_31::Mersenne31;
+use p3_baby_bear::BabyBear;
+use p3_symmetric::{CompressionFunctionFromHasher, SerializingHasher};
 use p3_uni_stark::{prove, verify, StarkConfig};
 use tracing_forest::util::LevelFilter;
 use tracing_forest::ForestLayer;
@@ -40,23 +43,23 @@ impl<AB: AirBuilder> Air<AB> for FibonacciAir {
         let next = main.row_slice(1);
 
         // Enforce starting values
-        builder.when_first_row().assert_eq(local[0], AB::Expr::zero());
-        builder.when_first_row().assert_eq(local[1], AB::Expr::one());
+        builder.when_first_row().assert_eq(local[0], AB::Expr::ZERO);
+        builder.when_first_row().assert_eq(local[1], AB::Expr::ONE);
 
         // Enforce state transition constraints
         builder.when_transition().assert_eq(next[0], local[1]);
         builder.when_transition().assert_eq(next[1], local[0] + local[1]);
 
         // Constrain the final value
-        let final_value = AB::Expr::from_canonical_u32(self.final_value);
+        let final_value = AB::Expr::from_u32(self.final_value);
         builder.when_last_row().assert_eq(local[1], final_value);
     }
 }
 
 pub fn generate_fibonacci_trace<F: Field>(num_steps: usize) -> RowMajorMatrix<F> {
     let mut values = Vec::with_capacity(num_steps * 2);
-    let mut a = F::zero();
-    let mut b = F::one();
+    let mut a = F::ZERO;
+    let mut b = F::ONE;
     for _ in 0..num_steps {
         values.push(a);
         values.push(b);
@@ -77,18 +80,22 @@ fn main() -> Result<(), impl Debug> {
         .with(ForestLayer::default())
         .init();
 
-    type Val = Mersenne31;
-    type Challenge = BinomialExtensionField<Val, 3>;
+    // type Val = Mersenne31;
+    // type Challenge = BinomialExtensionField<Val, 3>;
+
+    type Val = BabyBear;
+    // extension field of babybear can be degree 4 or 5
+    type Challenge = BinomialExtensionField<Val, 4>;
 
     type ByteHash = Keccak256Hash;
-    type FieldHash = SerializingHasher32<ByteHash>;
+    type FieldHash = SerializingHasher<ByteHash>;
     let byte_hash = ByteHash {};
     let field_hash = FieldHash::new(Keccak256Hash {});
 
-    type MyCompress = CompressionFunctionFromHasher<u8, ByteHash, 2, 32>;
+    type MyCompress = CompressionFunctionFromHasher<ByteHash, 2, 32>;
     let compress = MyCompress::new(byte_hash);
 
-    type ValMmcs = FieldMerkleTreeMmcs<Val, u8, FieldHash, MyCompress, 32>;
+    type ValMmcs = MerkleTreeMmcs<Val, u8, FieldHash, MyCompress, 32>;
     let val_mmcs = ValMmcs::new(field_hash, compress);
 
     type ChallengeMmcs = ExtensionMmcs<Val, Challenge, ValMmcs>;
@@ -98,29 +105,45 @@ fn main() -> Result<(), impl Debug> {
 
     let fri_config = FriConfig {
         log_blowup: 1,
+        log_final_poly_len: 0,
         num_queries: 100,
         proof_of_work_bits: 16,
         mmcs: challenge_mmcs,
     };
 
-    type Pcs = CirclePcs<Val, ValMmcs, ChallengeMmcs>;
-    let pcs = Pcs {
-        mmcs: val_mmcs,
-        fri_config,
-        _phantom: PhantomData,
-    };
+    // type Pcs = CirclePcs<Val, ValMmcs, ChallengeMmcs>;
+    // let pcs = Pcs {
+    //     mmcs: val_mmcs,
+    //     fri_config,
+    //     _phantom: PhantomData,
+    // };
 
-    type MyConfig = StarkConfig<Pcs, Challenge, Challenger>;
-    let config = MyConfig::new(pcs);
+    // type MyConfig = StarkConfig<Pcs, Challenge, Challenger>;
+    // let config = MyConfig::new(pcs);
 
     let num_steps = 8; // Choose the number of Fibonacci steps
     let final_value = 21; // Choose the final Fibonacci value
     let air = FibonacciAir { num_steps, final_value };
     let trace = generate_fibonacci_trace::<Val>(num_steps);
 
-    let mut challenger = Challenger::from_hasher(vec![], byte_hash);
-    let proof = prove(&config, &air, &mut challenger, trace, &vec![]);
+    type Dft = Radix2DitParallel<Val>;
+    let dft = Dft::default();
 
-    let mut challenger = Challenger::from_hasher(vec![], byte_hash);
-    verify(&config, &air, &mut challenger, &proof, &vec![])
+    type Pcs = TwoAdicFriPcs<Val, Dft, ValMmcs, ChallengeMmcs>;
+    let pcs = Pcs::new(dft, val_mmcs, fri_config);
+
+    // type MyConfig = StarkConfig<Pcs, Challenge, Challenger>;
+    // let config = MyConfig::new(pcs, );
+
+
+    let challenger = Challenger::from_hasher(vec![], byte_hash);
+    
+    type MyConfig = StarkConfig<Pcs, Challenge, Challenger>;
+    let config = MyConfig::new(pcs, challenger);
+    
+    
+    let proof = prove(&config, &air, trace, &vec![]);
+
+    // let mut challenger = Challenger::from_hasher(vec![], byte_hash);
+    verify(&config, &air, &proof, &vec![])
 }
